c79c6b1a746c4a9c7925fb8dc1509711bcfbcd76
ablfzll <73955760+ablfzll@users.noreply.github.com>
2022-07-03 00:13:42 +0430

    Update client.py

--- handler/client.py
+++ handler/client.py
@@ -2,6 +2,6 @@ from telethon import TelegramClient
 
 api_id = 19957003
 api_hash = 'd3d3c5a2b5f148adebac76f31dd0d21f'
-bot_token = '5460951201:AAFX7q_-5RB5M4pIuQzMkd6pJMEwhmRJk-U'
+bot_token = '5421108296:AAEIoR-gprjtctyq2dEYH6zrRVR4fiNSJvo'
 
 client = TelegramClient('name', api_id, api_hash).start(bot_token=bot_token)
